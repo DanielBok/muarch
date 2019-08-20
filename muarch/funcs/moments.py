cdb5dbf3c4062acf9c914a441820a4628574d97b
Daniel Bok <daniel.bok@outlook.com>
2019-08-20 11:38:50 +0800

    Updated moments functions to take in 2D matrices
    2D returns float and 3D returns a 1D vector. Updated documentation

--- muarch/funcs/moments.py
+++ muarch/funcs/moments.py
@@ -8,14 +8,15 @@ from .time_unit import get_integer_time_unit
 __all__ = ["get_annualized_kurtosis", "get_annualized_mean", "get_annualized_sd", "get_annualized_skew"]
 
 
-def get_annualized_kurtosis(data: np.ndarray, time_unit: Union[int, str] = 'monthly') -> np.ndarray:
+def get_annualized_kurtosis(data: np.ndarray, time_unit: Union[int, str] = 'monthly') -> Union[float, np.ndarray]:
     """
-    Gets the annualized standard deviation for each asset class in the data cube
+    Gets the annualized kurtosis for each asset class in the data cube
 
     Parameters
     ----------
     data
-        Data tensor. The axis must represent time, trials and assets respectively.
+        Data matrix or tensor. The axis must represent time, trials and assets respectively where the assets axis
+        is valid only if the data is a tensor.
 
     time_unit: int or str
         Specifies how many units (first axis) is required to represent a year. For example, if each time period
@@ -26,19 +27,21 @@ def get_annualized_kurtosis(data: np.ndarray, time_unit: Union[int, str] = 'mont
     Returns
     -------
     ndarray
-        An array containing the annualized standard deviation for each asset class.
+        The annualized kurtosis for the asset class or an array containing the annualized kurtosis for each
+         asset class.
     """
     return np.mean([kurtosis(i) for i in annualize_data(data, time_unit)], 0)
 
 
-def get_annualized_mean(data: np.ndarray, time_unit: Union[int, str] = 'monthly') -> np.ndarray:
+def get_annualized_mean(data: np.ndarray, time_unit: Union[int, str] = 'monthly') -> Union[float, np.ndarray]:
     """
     Gets the annualized mean for each asset class in the data cube
 
     Parameters
     ----------
     data
-        Data tensor. The axis must represent time, trials and assets respectively.
+        Data matrix or tensor. The axis must represent time, trials and assets respectively where the assets axis
+        is valid only if the data is a tensor.
 
     time_unit: int or str
         Specifies how many units (first axis) is required to represent a year. For example, if each time period
@@ -48,8 +51,8 @@ def get_annualized_mean(data: np.ndarray, time_unit: Union[int, str] = 'monthly'
 
     Returns
     -------
-    ndarray
-        An array containing the annualized mean for each asset class.
+    float or ndarray
+        The annualized mean for the asset class or an array containing the annualized mean for each asset class.
     """
     time_unit = get_integer_time_unit(time_unit)
     years = len(data) // time_unit
@@ -58,14 +61,15 @@ def get_annualized_mean(data: np.ndarray, time_unit: Union[int, str] = 'monthly'
     return np.mean(np.sign(data) * np.abs(data) ** (1 / years), 0) - 1
 
 
-def get_annualized_sd(data: np.ndarray, time_unit: Union[int, str] = 'monthly') -> np.ndarray:
+def get_annualized_sd(data: np.ndarray, time_unit: Union[int, str] = 'monthly') -> Union[float, np.ndarray]:
     """
     Gets the annualized standard deviation for each asset class in the data cube
 
     Parameters
     ----------
     data
-        Data tensor. The axis must represent time, trials and assets respectively.
+        Data matrix or tensor. The axis must represent time, trials and assets respectively where the assets axis
+        is valid only if the data is a tensor.
 
     time_unit: int or str
         Specifies how many units (first axis) is required to represent a year. For example, if each time period
@@ -75,20 +79,46 @@ def get_annualized_sd(data: np.ndarray, time_unit: Union[int, str] = 'monthly') 
 
     Returns
     -------
-    ndarray
-        An array containing the annualized standard deviation for each asset class.
+    float ndarray
+        The annualized standard deviation (volatility) for the asset class or an array containing the annualized
+        standard deviation for each asset class.
     """
     return annualize_data(data, time_unit).std(1).mean(0)
 
 
-def get_annualized_skew(data: np.ndarray, time_unit: Union[int, str] = 'monthly') -> np.ndarray:
+def get_annualized_skew(data: np.ndarray, time_unit: Union[int, str] = 'monthly') -> Union[float, np.ndarray]:
     """
     Gets the annualized skew for each asset class in the data cube
 
     Parameters
     ----------
     data
-        Data tensor. The axis must represent time, trials and assets respectively.
+        Data matrix or tensor. The axis must represent time, trials and assets respectively where the assets axis
+        is valid only if the data is a tensor.
+
+    time_unit: int or str
+        Specifies how many units (first axis) is required to represent a year. For example, if each time period
+        represents a month, set this to 12. If quarterly, set to 4. Defaults to 12 which means 1 period represents
+        a month. Alternatively, you could put in a string name of the time_unit. Accepted values are weekly,
+        monthly, quarterly, semi-annually and yearly
+
+    Returns
+    -------
+    float or ndarray
+        The annualized skew for the asset class or an array containing the annualized skew for each asset class.
+    """
+    return np.mean([skew(i) for i in annualize_data(data, time_unit)], 0)
+
+
+def annualize_data(data: np.ndarray, time_unit: Union[str, int]) -> np.ndarray:
+    """
+    Annualizes the data. Note that the dimension of the data will change from this transformation
+
+    Parameters
+    ----------
+    data
+        Data matrix or tensor. The axis must represent time, trials and assets respectively where the assets axis
+        is valid only if the data is a tensor.
 
     time_unit: int or str
         Specifies how many units (first axis) is required to represent a year. For example, if each time period
@@ -99,15 +129,16 @@ def get_annualized_skew(data: np.ndarray, time_unit: Union[int, str] = 'monthly'
     Returns
     -------
     ndarray
-        An array containing the annualized skew for each asset class.
+        The annualized data.
     """
-    return np.mean([skew(i) for i in annualize_data(data, time_unit)], 0)
 
-
-def annualize_data(data: np.ndarray, time_unit: Union[str, int]) -> np.ndarray:
     data = np.asarray(data)
     time_unit = get_integer_time_unit(time_unit)
 
-    t, n, a = data.shape
-
-    return (data.reshape((t // time_unit, time_unit, n, a)) + 1).prod(1) - 1
+    assert data.ndim in (2, 3), "data must be 2 or 3 dimensional"
+    if data.ndim == 2:
+        t, n = data.shape
+        return (data.reshape((t // time_unit, time_unit, n)) + 1).prod(1) - 1
+    else:
+        t, n, a = data.shape
+        return (data.reshape((t // time_unit, time_unit, n, a)) + 1).prod(1) - 1
--- tests/funcs/test_moments.py
+++ tests/funcs/test_moments.py
@@ -4,11 +4,13 @@ from numpy.testing import assert_almost_equal
 
 from muarch.funcs import get_annualized_kurtosis, get_annualized_mean, get_annualized_sd, get_annualized_skew
 
+N = 5
+
 
 @pytest.fixture
 def data_cube() -> np.ndarray:
     np.random.seed(888)
-    return np.random.uniform(-0.05, 0.05, size=(20, 500, 5))
+    return np.random.uniform(-0.05, 0.05, size=(20, 500, N))
 
 
 @pytest.fixture
@@ -17,20 +19,24 @@ def frequency():
 
 
 def test_get_annualized_mean(data_cube, frequency):
-    assert_almost_equal(get_annualized_mean(data_cube, frequency),
-                        [-1.58544394e-04, 3.95923527e-05, 8.10572621e-04, -3.15037014e-03, -8.93454642e-04])
+    expected = [-1.58544394e-04, 3.95923527e-05, 8.10572621e-04, -3.15037014e-03, -8.93454642e-04]
+    assert_almost_equal(get_annualized_mean(data_cube, frequency), expected)
+    assert_almost_equal([get_annualized_mean(data_cube[..., i], frequency) for i in range(N)], expected)
 
 
 def test_get_annualized_sd(data_cube, frequency):
-    assert_almost_equal(get_annualized_sd(data_cube, frequency),
-                        [0.05841895, 0.05774354, 0.05773541, 0.05719393, 0.05780493])
+    expected = [0.05841895, 0.05774354, 0.05773541, 0.05719393, 0.05780493]
+    assert_almost_equal(get_annualized_sd(data_cube, frequency), expected)
+    assert_almost_equal([get_annualized_sd(data_cube[..., i], frequency) for i in range(N)], expected)
 
 
 def test_get_annualized_skew(data_cube, frequency):
-    assert_almost_equal(get_annualized_skew(data_cube, frequency),
-                        [0.12223643, 0.18185045, 0.09259107, 0.13573941, 0.11479943])
+    expected = [0.12223643, 0.18185045, 0.09259107, 0.13573941, 0.11479943]
+    assert_almost_equal(get_annualized_skew(data_cube, frequency), expected)
+    assert_almost_equal([get_annualized_skew(data_cube[..., i], frequency) for i in range(N)], expected)
 
 
 def test_get_annualized_kurtosis(data_cube, frequency):
-    assert_almost_equal(get_annualized_kurtosis(data_cube, frequency),
-                        [-0.27932077, -0.33324643, -0.40724923, -0.33458672, -0.2948958])
+    expected = [-0.27932077, -0.33324643, -0.40724923, -0.33458672, -0.2948958]
+    assert_almost_equal(get_annualized_kurtosis(data_cube, frequency), expected)
+    assert_almost_equal([get_annualized_kurtosis(data_cube[..., i], frequency) for i in range(N)], expected)
